8b3951c1ca578f48e791d2e491ff96b73c520b61
Kiran TJ <kiran.thomas.jacob@gmail.com>
2024-10-09 18:44:06 +0530

    Commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+    const [showPara, setShowPara] = useState(false);
+
+    return (
+        <div id="main">
+            {/* // Do not alter the main div */}
+            <button id="click" onClick={() => (setShowPara(!showPara))}>{showPara ? "Hide Paragraph" : "Show Paragraph"}</button>
+            {
+                showPara &&
+                <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+            }
+        </div>
+    );
 }
 
 
